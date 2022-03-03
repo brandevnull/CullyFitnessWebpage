969d2ac307a74b5768ac0ae0646b382a9335987c
Brandon Larkin <brandonlarkin@gmail.com>
2022-03-03 14:44:25 -0800

    revert some stuff

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
+import logo from './logo.svg';
 import './App.css';
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import Client from './Models/Client';
 import Helmet from 'react-helmet';
 
 import {
   BrowserRouter as Router,
   Switch,
   Route,
+  Link,
+  Redirect,
+  useHistory,
+  withRouter
 } from "react-router-dom";
 import Workout from './Components/Workout/Workout'
+import { useState, useEffect } from 'react';
+import Exercise from './Models/Exercise';
+import Select from 'react-select'
+import { Button } from '@mui/material';
 import NameSelect from './Components/NameSelect/NameSelect';
 import ImportClients from './Pages/ImportClients';
 
 function App() {
+
+  const firebaseApp = initializeApp({
+    apiKey: 'AIzaSyBoQT4L3shuLfXGgQeQKR6jv2V0zA-Xnk0',
+    authDomain: 'cullyfitness.firebaseapp.com',
+    projectId: 'cullyfitness'
+  });
 
   return (
     <Router>
